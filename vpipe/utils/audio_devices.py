496b3055d6e31c65301599b6e380c4706905befb
duynv <duynguyen.fet@gmail.com>
2025-06-30 09:14:17 +0700

    feat: enhance device listing with filtering ns team vitual device

--- vpipe/utils/audio_devices.py
+++ vpipe/utils/audio_devices.py
@@ -1,5 +1,25 @@
 import sounddevice as sd
 
+"""
+Default, we exclude "NS Team" virtual devices
+"""
+
+def default_input_filter(name, dev):
+    return (
+        dev.get('hostapi', -1) == get_wasapi_index()
+        and dev.get('max_input_channels', 0) > 0
+        and (not 'ns team' in name.lower())
+    )
+
+
+def default_output_filter(name, dev):
+    return (
+        dev.get('hostapi', -1) == get_wasapi_index()
+        and dev.get('max_output_channels', 0) > 0
+        and (not 'ns team' in name.lower())
+    )
+
+
 def get_wasapi_index():
     hostapis = sd.query_hostapis()
     for idx, api in enumerate(hostapis):
@@ -7,52 +27,63 @@ def get_wasapi_index():
             return idx
     raise RuntimeError("WASAPI not found on this system.")
 
-def list_input_devices():
+
+def list_input_devices(filter=None):
     """
-    Result: List[Tuple[index, name]]
+    Returns: List[Tuple[index, name]]
+    filter: callback(name:str, dev:dict)->bool, if provided, will be used to filter devices
     """
-    wasapi = get_wasapi_index()
+    if filter is None:
+        filter = default_input_filter
     devices = sd.query_devices()
     result = []
     for idx, dev in enumerate(devices):
-        if dev['hostapi'] == wasapi and dev['max_input_channels'] > 0:
+        if filter is None or filter(dev['name'], dev):
             result.append((idx, dev['name']))
     return result
 
-def list_output_devices():
+
+def list_output_devices(filter=None):
     """
-    Result: List[Tuple[index, name]]
+    Returns: List[Tuple[index, name]]
+    filter: callback(name:str, dev:dict)->bool, if provided, will be used to filter devices
     """
-    wasapi = get_wasapi_index()
+    if filter is None:
+        filter = default_output_filter
     devices = sd.query_devices()
     result = []
     for idx, dev in enumerate(devices):
-        if dev['hostapi'] == wasapi and dev['max_output_channels'] > 0:
+        if filter is None or filter(dev['name'], dev):
             result.append((idx, dev['name']))
     return result
 
-def find_device_index(name, is_input=None):
+
+def find_device_index(name, is_input=None, filter=None):
     """
     Find the index of a device by name.
     If is_input = True: search only input devices.
     If is_input = False: search only output devices.
     If is_input = None: search all devices.
+    filter: callback(name:str, dev:dict)->bool, if provided, will be used to filter devices
     """
+    if is_input is True and filter is None:
+        filter = default_input_filter
+    if is_input is False and filter is None:
+        filter = default_output_filter
     devices = sd.query_devices()
     for idx, dev in enumerate(devices):
+        if filter is not None and not filter(dev['name'], dev):
+            continue
         if dev['name'] == name:
-            if is_input is True and dev['max_input_channels'] == 0:
-                continue
-            if is_input is False and dev['max_output_channels'] == 0:
-                continue
             return idx
     raise ValueError(f"Device named '{name}' not found.")
 
+
 if __name__ == "__main__":
-    print("=== Input Devices (WASAPI) ===")
+    print(">> Input Devices (WASAPI)")
     for idx, name in list_input_devices():
         print(f"[{idx}] {name}")
 
-    print("\n=== Output Devices (WASAPI) ===")
+    print("\n>> Output Devices (WASAPI)")
     for idx, name in list_output_devices():
         print(f"[{idx}] {name}")
